472741bc4f5c340758104961dd732e05b2362e3e
RajivGM <48874839+RajivGM@users.noreply.github.com>
2021-03-09 22:07:01 +0530

    Create ProjectImages.js

---
+++ ProjectImages/ProjectImages.js
@@ -0,0 +1,24 @@
+const Images = {
+    logo:require('../Images/logo.png'),
+    imput_file:require('../Images/imageuplod.png'),
+    wave_hand:require('../Images/ic_wave_hand.png'),
+    send:require('../Images/send.png'),
+    stiker:require('../Images/sterkar.png'),
+    choosefile:require('../Images/icon.png'),
+    emoji1:require('../Images/1happy.png'),
+    emoji2:require('../Images/2Emoji.png'),
+    emoji3:require('../Images/3Emoji.png'),
+    emoji4:require('../Images/4Emoji.jpeg'),
+    emoji5:require('../Images/5Emoji.png'),
+    emoji6:require('../Images/6Emoji.png'),
+    mimi1: require('../Images/mimi1.gif'),
+    mimi2: require('../Images/mimi2.gif'),
+    mimi3: require('../Images/mimi3.gif'),
+    mimi4: require('../Images/mimi4.gif'),
+    mimi5: require('../Images/mimi5.gif'),
+    mimi6: require('../Images/mimi6.gif'),
+    mimi7: require('../Images/mimi7.gif'),
+    mimi8: require('../Images/mimi8.gif'),
+    mimi9: require('../Images/mimi9.gif')
+}
+export default Images
